53372e0394d01d864e6e4a2ea3a5c106fc0f77f0
JJ <jj.vega86@gmail.com>
2022-06-27 06:38:22 -0400

    first commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,24 @@
+<!-- 
+import { onMount, onDestroy } from "svelte";
+let feedback = [];
+
+const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data));
+
+onDestroy(() => {
+  unsubscribe();
+});
+
+ -->
+<script>
+  import { FeedbackStore } from "../store/stores";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+</script>
+
+<!-- * LONG WAY TO SUBSCRIBE/UNSUBSCRIBE TO A STORE ABOVE -->
+
+{#each $FeedbackStore as item (item.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem {item} />
+  </div>
+{/each}
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { FeedbackStore } from "../store/stores";
+
+  $: count = $FeedbackStore.length;
+  $: average =
+    $FeedbackStore.length > 0
+      ? $FeedbackStore.reduce((total, item) => {
+          return total + item.rating;
+        }, 0) / count
+      : 0;
+</script>
+
+<div class="feedback-stats">
+  <h4>{count} Reviews</h4>
+  <h4>Ratings Average: {average}</h4>
+</div>
+
+<style>
+  .feedback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
